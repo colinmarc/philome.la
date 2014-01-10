a62f84669f1142a9836debb0e98da25474c33a38
Colin Marc <colinmarc@gmail.com>
2014-01-10 20:07:18 +0000

    fix twine verification for v2

--- verify_twine.js
+++ verify_twine.js
@@ -3,7 +3,7 @@ var system = require('system'),
 
 page.open(system.args[1], function () {
     var is_twine = page.evaluate(function() {
-      return ((window['Tale'] !== undefined) || (window['tale'] !== undefined))
+      return ((window['Tale'] !== undefined) || (window['tale'] !== undefined) || (window['story'] !== undefined))
     });
 
     var exit_code = is_twine ? 0 : 1;
